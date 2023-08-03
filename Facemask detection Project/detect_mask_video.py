aec1335b64b76f17824f5e3ae26bb34644810452
SaikiranVoladri <saikiranreddy01818@gmail.com>
2023-08-03 16:44:58 +0530

    all clear

--- Facemask detection Project/detect_mask_video.py
+++ Facemask detection Project/detect_mask_video.py
@@ -72,12 +72,12 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	return (locs, preds)
 
 # load our serialized face detector model from disk
-prototxtPath = r"face_detector\deploy.prototxt"
-weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
+prototxtPath = r"S:\ML(projects)\Facemask detection Project\face_detector\deploy.prototxt"
+weightsPath = r"S:\ML(projects)\Facemask detection Project\face_detector\res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 # load the face mask detector model from disk
-maskNet = load_model("mask_detector.model")
+maskNet = load_model("S:\ML(projects)\Facemask detection Project\mask_detector.model2")
 
 # initialize the video stream
 print("[INFO] starting video stream...")
@@ -88,7 +88,8 @@ while True:
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
 	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+	
+	frame = imutils.resize(frame, width=700)
 
 	# detect faces in the frame and determine if they are wearing a
 	# face mask or not
