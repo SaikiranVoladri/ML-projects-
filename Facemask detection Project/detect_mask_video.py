9f00c53ee261fb08492327f44f513d53f413ae3e
SaikiranVoladri <saikiranreddy01818@gmail.com>
2023-08-03 17:17:59 +0530

    updated all

--- Facemask detection Project/detect_mask_video.py
+++ Facemask detection Project/detect_mask_video.py
@@ -64,8 +64,8 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 		# for faster inference we'll make batch predictions on *all*
 		# faces at the same time rather than one-by-one predictions
 		# in the above `for` loop
-		faces = np.array(faces, dtype="float32")
-		preds = maskNet.predict(faces, batch_size=32)
+		faces = np.array(faces, dtype="float64")
+		preds = maskNet.predict(faces, batch_size=64)
 
 	# return a 2-tuple of the face locations and their corresponding
 	# locations
@@ -81,48 +81,56 @@ maskNet = load_model("S:\ML(projects)\Facemask detection Project\mask_detector.m
 
 # initialize the video stream
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
+vs = VideoStream(src=0, backend=cv2.CAP_DSHOW).start()
 
 # loop over the frames from the video stream
+# loop over the frames from the video stream
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
-	
-	frame = imutils.resize(frame, width=700)
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
 
-	# detect faces in the frame and determine if they are wearing a
-	# face mask or not
-	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+    # Check if the frame is None
+    if frame is None:
+        # If frame is None, it means there was an issue reading from the video stream
+        # You can handle this situation as needed, e.g., break the loop or display an error message
+        print("Error: Unable to read frame from video stream.")
+        break
 
-	# loop over the detected face locations and their corresponding
-	# locations
-	for (box, pred) in zip(locs, preds):
-		# unpack the bounding box and predictions
-		(startX, startY, endX, endY) = box
-		(mask, withoutMask) = pred
+    frame = imutils.resize(frame, width=600)
 
-		# determine the class label and color we'll use to draw
-		# the bounding box and text
-		label = "Mask" if mask > withoutMask else "No Mask"
-		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+    # detect faces in the frame and determine if they are wearing a
+    # face mask or not
+    (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
 
-		# include the probability in the label
-		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+    # loop over the detected face locations and their corresponding
+    # locations
+    for (box, pred) in zip(locs, preds):
+        # unpack the bounding box and predictions
+        (startX, startY, endX, endY) = box
+        (mask, withoutMask) = pred
 
-		# display the label and bounding box rectangle on the output
-		# frame
-		cv2.putText(frame, label, (startX, startY - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+        # determine the class label and color we'll use to draw
+        # the bounding box and text
+        label = "Mask" if mask > withoutMask else "No Mask"
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+        # include the probability in the label
+        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+        # display the label and bounding box rectangle on the output
+        # frame
+        cv2.putText(frame, label, (startX, startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("a"):
+        break
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
